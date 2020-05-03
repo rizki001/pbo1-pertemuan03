72ee587023636674f6483d9288171a8b3a334568
rizki001 <milky_mw001@yahoo.com>
2020-05-03 22:54:36 +0800

    Operator If Else dengan condition False dengan statement 2 jalan

--- src/operator/IfElse.java
+++ src/operator/IfElse.java
@@ -2,14 +2,14 @@ package operator;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
-        if (uangDiDompet < totalBelanja) {
+        if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi belanjaan anda");
 
-        } else {
-            System.out.println("Uang cukup, selamat menikmati");
+        }else {
+            System.out.println("Uang cukup, Terima kasih");
         }
     }
 }
