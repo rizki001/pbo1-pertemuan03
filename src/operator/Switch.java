a6c7c48e0ae88da37a850d269de0d596558061df
rizki001 <milky_mw001@yahoo.com>
2020-05-03 23:33:18 +0800

    Operator Switch dengan Char

--- src/operator/Switch.java
+++ src/operator/Switch.java
@@ -2,24 +2,29 @@ package operator;
 
 public class Switch {
     public static void main(String[] args) {
-        int jalanJalan=3;
-        switch(jalanJalan){
-            case 1:
-                System.out.println("Orang Free");
+        char nilai='D';
+        switch(nilai) {
+            case 'A':
+                System.out.println("Mahasiswa	: \"Terima kasih pak\"");
+                System.out.println("Dosen	: \"Selamat ya...\"");
                 break;
-            case 2:
-                System.out.println("Orang Pacaran");
+            case 'B':
+                System.out.println("Mahasiswa	: \"Kenapa saya tidak mendapat nilai A, pak?\"");
+                System.out.println("Dosen	: \"&*^%$#\"");
                 break;
-            case 3:
-                System.out.println("Orang ketiga");
-                break;
-            case 4:
-                System.out.println("Double Date");
+            case 'C':
+                System.out.println("Mahasiswa	: \"Kenapa saya menurun, padahal saya selalu mengumpul tugas\"");
+                System.out.println("Dosen	: \"Tapi apakah bisa menjawab soal ujian?\"");
+                System.out.println("Mahasiswa	: \"hehehe...\"");
                 break;
             default:
-                System.out.println("Setan anjay");
+                System.out.println("Mahasiswa	: \"saya selalu masuk dan tugas saya mengumpul semua, kenapa nilai saya cuman segini, pak?\"");
+                System.out.println("Dosen	: \"bener? coba bapak cek dulu..\"");
+                System.out.println("Dosen	: \"*memeriksa berkas\"");
+                System.out.println("Dosen	: \"*marah\"");
+                System.out.println("Mahasiswa	: \"hehe, ampun pak...\"");
                 break;
 
+            }
         }
     }
-}
