26839a3793d5b9fbba131064088a0d155e231a23
rizki001 <milky_mw001@yahoo.com>
2020-05-03 23:27:00 +0800

    Operator Switch

---
+++ src/operator/Switch.java
@@ -0,0 +1,25 @@
+package operator;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("Orang Free");
+                break;
+            case 2:
+                System.out.println("Orang Pacaran");
+                break;
+            case 3:
+                System.out.println("Orang ketiga");
+                break;
+            case 4:
+                System.out.println("Double Date");
+                break;
+            default:
+                System.out.println("Setan anjay");
+                break;
+
+        }
+    }
+}
