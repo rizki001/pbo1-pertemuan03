fe2b18056e370f76309253ee74facd79ded7c312
rizki001 <milky_mw001@yahoo.com>
2020-05-03 21:48:50 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,12 @@ public class OperatorBinary {
     String nama = "Rizki Rahmawati";
     int jumlahSks = 24;
     double indeksPrestasiKomulatif = 3.57;
+    public static void main(String[] args) {
+        System.out.println(9 + 3);
+        System.out.println(9 - 3);
+        System.out.println(9 * 3);
+        System.out.println(9 / 3);
+        System.out.println(9 % 3);
+        System.out.println(9 % 2);
+    }
 }
