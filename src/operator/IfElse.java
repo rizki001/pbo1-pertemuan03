2168491c58de17c259194c886a722f6791f257e6
rizki001 <milky_mw001@yahoo.com>
2020-05-03 22:50:09 +0800

    Operator If Else dengan condition True dengan 1 statement jalan

---
+++ src/operator/IfElse.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
+
+        if (uangDiDompet < totalBelanja) {
+            System.out.println("Uang kurang, kurangi belanjaan anda");
+
+        } else {
+            System.out.println("Uang cukup, selamat menikmati");
+        }
+    }
+}
