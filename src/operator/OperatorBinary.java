26c80f978aa72d9983b00154b759c852aabb0b54
rizki001 <milky_mw001@yahoo.com>
2020-05-03 21:42:35 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalakan oleh program
+
+    String nama = "Rizki Rahmawati";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.57;
+}
