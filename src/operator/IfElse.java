af25d6cfd3ac45b3e3714ba764e8f1bc431ccdda
rizki001 <milky_mw001@yahoo.com>
2020-05-03 23:00:05 +0800

    Operator If Else dengan condition1 True dan condition2 false sehingga statement1 jalan

--- src/operator/IfElse.java
+++ src/operator/IfElse.java
@@ -2,14 +2,18 @@ package operator;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi belanjaan anda");
 
-        }else {
-            System.out.println("Uang cukup, Terima kasih");
+        }else if(uangDiDompet>totalBelanja){
+            double kembalian = uangDiDompet-totalBelanja;
+
+            System.out.println("Uang cukup, dengan kembalian : "+kembalian);
+        }else{
+            System.out.println("Uang pas, Alhamdulillah...");
         }
     }
 }
